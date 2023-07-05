317e7935598038c771ac47582e5ad8971f9cbbe2
Adil Ahamed <adilahamed189@gmail.com>
2023-07-05 20:53:56 +0530

    commanded jquery

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'jquery/dist/jquery.min.js';
-import 'popper.js/dist/popper.min.js';
+// import 'jquery/dist/jquery.min.js';
+// import 'popper.js/dist/popper.min.js';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
 import './index.scss';
 import App from './App';
