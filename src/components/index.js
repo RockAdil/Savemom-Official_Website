10922b0b139f383670b80087f4896583526760d2
Adil Ahamed <adilahamed189@gmail.com>
2023-07-05 21:00:00 +0530

    commanded whoweare

--- src/components/index.js
+++ src/components/index.js
@@ -4,4 +4,4 @@ export { default as HowWeWork } from './HowWeWork/howWeWork';
 export { default as NewsReports } from './News&Reports/newsReports';
 export { default as OurJourney } from './OurJourney/ourJourney';
 export { default as Product } from './Product/product';
-export { default as WhoWeAre } from './WhoWeAre/whoWeAre';
+// export { default as WhoWeAre } from './WhoWeAre/whoWeAre';
