c4fb84340c3f55ef430a563dad2e2ff4d1dd8a2d
Adil Ahamed <adilahamed189@gmail.com>
2023-07-08 12:44:14 +0530

    whoweare__content-3 Created

--- src/components/WhoWeAre/whoWeAre.jsx
+++ src/components/WhoWeAre/whoWeAre.jsx
@@ -1,9 +1,61 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import Navbar from '../../containers/Navbar/Navbar';
+import { BsArrowLeftShort, BsArrowRightShort } from 'react-icons/bs';
 
 import './whoWeAre.scss';
 
+const youtubeVideos = [
+  {
+    src: 'https://www.youtube.com/embed/Z_374EOhCAs',
+    title:
+      'Artificial Intelligence for Villages | Senthil Kumar M | TEDxBITSathy',
+  },
+  {
+    src: 'https://www.youtube.com/embed/YjXAuVejpT4',
+    title:
+      'Innovation India 2018 | A Platform for Emerging Entrepreneurs | Episode-7 | DD National',
+  },
+  {
+    src: 'https://www.youtube.com/embed/OYHm7-_MpKQ',
+    title: 'கல்லாதது உலகளவு 16-06-2018 - JAYAPLUS',
+  },
+  {
+    src: 'https://www.youtube.com/embed/lIEm7xuGels',
+    title: 'SaveMom',
+  },
+  {
+    src: 'https://www.youtube.com/embed/lpPBnus4Y3w',
+    title:
+      'The Vault | Pitch - Save Mom - The latest must have wearable device for Pregnant Women',
+  },
+  {
+    src: 'https://www.youtube.com/embed/SpiF6rJ_dMo',
+    title: 'Savemom: technology with social impact- Meet Mr.  Senthil kumar',
+  },
+  {
+    src: 'https://www.youtube.com/embed/z1ggAMB9hc4',
+    title:
+      'Olive Wear with its wearable products is helping expectant mothers during their journey',
+  },
+  {
+    src: 'https://www.youtube.com/embed/DMi4xBCIi6o',
+    title: 'SaveMom, JioVio Healthcare - Ms  Divya Krishnan',
+  },
+];
+
 const WhoWeAre = () => {
+  const scrollRef = useRef(null);
+
+  const scroll = (direction) => {
+    const { current } = scrollRef;
+
+    if (direction === 'left') {
+      current.scrollLeft -= 300;
+    } else {
+      current.scrollLeft += 300;
+    }
+  };
+
   return (
     <>
       <Navbar nav_bgcolor='' nav_color='navbar-dark' />
@@ -121,6 +173,41 @@ const WhoWeAre = () => {
               </p>
             </div>
           </div>
+        </div>
+      </div>
+
+      {/* CONTENT - 3 */}
+      <div className='whoweare__content-3 section__padding'>
+        <h3 className='sub_head-text' style={{ textAlign: 'center' }}>
+          Impact of Savemom
+        </h3>
+
+        <div className='content-3_youtube'>
+          <div className='content-3_youtube_container' ref={scrollRef}>
+            {youtubeVideos.map((video, index) => (
+              <div className='content-3_youtube-video' key={`video - ${index}`}>
+                <iframe
+                  width='367'
+                  height='250'
+                  src={video.src}
+                  title={video.title}
+                  allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+                  allowFullScreen
+                ></iframe>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <div className='content-3_youtube-arrors'>
+          <BsArrowLeftShort
+            className='video__arror-icon'
+            onClick={() => scroll('left')}
+          />
+          <BsArrowRightShort
+            className='video__arror-icon'
+            onClick={() => scroll('right')}
+          />
         </div>
       </div>
     </>
